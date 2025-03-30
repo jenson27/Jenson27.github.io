a57c985fa41030bf2113290ee9eb7fb84e22878e
Astral <jenson27@Hotmail.com>
2025-03-30 14:22:50 +0800

    Edited majority of stuff

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = "Jenson | Personal Portfolio";
+export const SITE_DESCRIPTION =
+    "I am a dedicated programmer with a deep passion for problem-solving and innovation. For me, coding is more than just writing lines of code—it’s about creating meaningful solutions and pushing the boundaries of technology. I thrive on challenges, constantly exploring new languages, frameworks, and methodologies to enhance my skills. Continuous learning is at the core of my journey. Whether it’s mastering new technologies, optimizing algorithms, or diving into emerging trends like AI and cloud computing, I embrace every opportunity to grow and adapt. My goal is to stay ahead in the ever-evolving tech landscape, refining my expertise while building impactful projects. With a mindset driven by curiosity and persistence, I approach each project with enthusiasm, problem-solving skills, and a commitment to excellence.";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
